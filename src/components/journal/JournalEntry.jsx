4a9012e4f875edbef238b83ad5aa5261675e5445
Eparedes456 <erickparedes456@gmail.com>
2022-01-11 10:27:01 -0500

    dd
    dd

---
+++ src/components/journal/JournalEntry.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry pointer">
+            <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize:'cover',
+                    backgroundImage :'url(https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg)'
+                }}
+            ></div>
+
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">Un nuevo día</p>
+                <p className="journal__entry-content">
+                    Un nuevo díadsfsdfsdfsdfasd
+                </p>
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+
+        </div>
+    )
+}
